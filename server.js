b74b8a21c99378e6577e120d22fb1e54d8d1a184
Richard <ogb22001@byui.edu>
2025-09-27 23:25:47 +0100

    set up get, post, put,  delete methods and installed swagger

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.emv || 3000;
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database')
 
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
@@ -15,3 +27,5 @@ mongodb.initDb((err) => {
         });
     }
 })
+
+
